3057152af3ee1446af70cb059c90b9583d1f330b
maybezzz777 <131018266+maybezzz777@users.noreply.github.com>
2023-11-30 17:40:15 +0100

    Add files via upload

--- text.js
+++ text.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var typed = new Typed(".change", {
-        strings: ["Racist", "Horny", "Homophobic", "Acoustic", "Sexist"],
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
+  var typed = new Typed(".change", {
+    strings: ["Racist", "Homophobic", "Acoustic", "Sexist"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var checkbox = document.getElementById('nav-toggle');
-    checkbox.addEventListener('change', function () {
-        document.body.classList.toggle('hide-content', checkbox.checked);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var checkbox = document.getElementById("nav-toggle");
+  checkbox.addEventListener("change", function () {
+    document.body.classList.toggle("hide-content", checkbox.checked);
+  });
 });
-
